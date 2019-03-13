557a2750aafa2e039145f11ed7a839582f488ce7
divye singh <sdivye92@gmail.com>
2019-03-13 19:11:58 +0530

    Modified update map function for angle in degree and added global variables

--- mapping-AI/mapping/components/constants.py
+++ mapping-AI/mapping/components/constants.py
@@ -1,6 +1,15 @@
 import math
 import numpy as np
 
+MAX_RANGE = 10
+METRE_2_PIX = 5
+DELTA_R = 0.1
+
+cos = lambda x: np.cos(np.deg2rad(x))
+sin = lambda x: np.sin(np.deg2rad(x))
+
+len2ind = lambda cPos, rad, func, alpha: math.floor( (cPos + rad * func(alpha)) * METRE_2_PIX )
+
 class Directions:
     NORTH = (0, 1)
     NE = (1, 1)
@@ -11,6 +20,7 @@ class Directions:
     WEST = (-1, 0)
     NW = (-1, 1)
     DELTA_THETA = 0.5
+    METRE_2_PIX = 5
 
 
 class Action:
@@ -42,25 +52,26 @@ class Action:
         xc, yc = current
         new_internal_map = internal_map
         x, y = direction
-        directionAngle = math.atan(y / x)
+        directionAngle = np.rad2deg(math.atan2(y, x))
+        if np.sign(directionAngle) == -1:
+            directionAngle += 360
 
-        maxr = 10
-        delta_r = 0.5
+        maxr = MAX_RANGE
 
         size_sensory_array = len(sensory_array)
         current_angle = directionAngle - ((size_sensory_array/2) * Directions.DELTA_THETA)
 
         for scan in sensory_array:
             if scan != math.inf:
-                new_internal_map[xc + radius * math.cos(current_angle)][
-                    yc + radius * math.sin(current_angle)] = 1
                 radius = scan
+                new_internal_map[len2ind(xc, radius, cos, current_angle)][
+                    len2ind(yc, radius, sin, current_angle)] = 1
             else:
                 radius = maxr
 
-            for i in np.arange(0, radius, delta_r):
-                new_internal_map[xc + i * math.cos(current_angle)][
-                    yc + i * math.sin(current_angle)] = 0
+            for i in np.arange(0, radius, DELTA_R):
+                new_internal_map[len2ind(xc, i, cos, current_angle)][
+                    len2ind(yc, i, sin, current_angle)] = 0
 
             current_angle += Directions.DELTA_THETA
 
